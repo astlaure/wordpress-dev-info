a16a40e3734069bcbea62d05dc6c2204e7294d41
Alexandre St-Laurent <alexandre.stlaurent.4@gmail.com>
2020-12-13 13:39:03 -0500

    switch dev to test for chrome

--- scripts/setup-hosts.bat
+++ scripts/setup-hosts.bat
@@ -2,8 +2,8 @@
 
 SET NEWLINE=^& echo.
 
-FIND /C /I "dev-staging.dev" %WINDIR%\system32\drivers\etc\hosts
-IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^192.168.2.23 dev-staging.dev>>%WINDIR%\System32\drivers\etc\hosts
+FIND /C /I "dev-staging.test" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^192.168.2.23 dev-staging.test>>%WINDIR%\System32\drivers\etc\hosts
 
 FIND /C /I "hobbies-staging.dev" %WINDIR%\system32\drivers\etc\hosts
-IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^192.168.2.23 hobbies-staging.dev>>%WINDIR%\System32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^192.168.2.23 hobbies-staging.test>>%WINDIR%\System32\drivers\etc\hosts
