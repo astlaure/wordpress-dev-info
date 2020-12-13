c78d938fa9320bb13d56f3763720ec0ddf713c53
Alexandre St-Laurent <alexandre.stlaurent.4@gmail.com>
2020-12-13 13:39:18 -0500

    forgot one

--- scripts/setup-hosts.bat
+++ scripts/setup-hosts.bat
@@ -5,5 +5,5 @@ SET NEWLINE=^& echo.
 FIND /C /I "dev-staging.test" %WINDIR%\system32\drivers\etc\hosts
 IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^192.168.2.23 dev-staging.test>>%WINDIR%\System32\drivers\etc\hosts
 
-FIND /C /I "hobbies-staging.dev" %WINDIR%\system32\drivers\etc\hosts
+FIND /C /I "hobbies-staging.test" %WINDIR%\system32\drivers\etc\hosts
 IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^192.168.2.23 hobbies-staging.test>>%WINDIR%\System32\drivers\etc\hosts
